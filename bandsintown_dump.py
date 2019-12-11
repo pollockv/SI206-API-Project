87187466c2dffefc5787456fbeb3a94b339dfac7
Virginia Pollock <pollockv@umich.edu>
2019-12-10 19:01:34 -0500

    added artistinfo function

--- bandsintown_dump.py
+++ bandsintown_dump.py
@@ -4,17 +4,25 @@ import sqlite3
 import json
 import os
 import sys
+import re
+import urllib
+import bandsintown
 
-# https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction
+# https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0#/VenueData
 
-def get_recommendations_from_tastedive(bandName, key="349890-SI206Fin-N4RHDBVP"):
-    baseurl="https://tastedive.com/api/similar"
-    params_d = {}
-    params_d["q"]= bandName
-    params_d["k"]= key
-    params_d["type"]= "music"
-    params_d["limit"] = "20"
-    resp = requests.get(baseurl, params=params_d)
-    print(resp.url)
-    respDic = resp.json()
-    return respDic
+def get_bandsintown_artistinfo(bandName, id="75771b0391833569dedd2b5ceff8d2af"):
+    bandsdict = {}
+    base_url = "https://rest.bandsintown.com/artists/" + bandName + "?app_id=" + id
+    artistinfo = requests.get(base_url)
+    print(artistinfo.url)
+    return bandsdict
+
+
+def main():
+    artists = ["Drake", "Hippo Campus", "STRFKR", "Mac Demarco"]
+
+    for artist in artists:
+        print(get_bandsintown_artistinfo(artist))
+
+if __name__ == "__main__":
+    main()
