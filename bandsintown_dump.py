d69a5baa2baa7a760fa357d55eb41291520f1241
Virginia Pollock <pollockv@umich.edu>
2019-12-10 22:17:18 -0500

    added main method and method for finding event info

--- bandsintown_dump.py
+++ bandsintown_dump.py
@@ -11,18 +11,26 @@ import bandsintown
 # https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0#/VenueData
 
 def get_bandsintown_artistinfo(bandName, id="75771b0391833569dedd2b5ceff8d2af"):
-    bandsdict = {}
     base_url = "https://rest.bandsintown.com/artists/" + bandName + "?app_id=" + id
-    artistinfo = requests.get(base_url)
-    print(artistinfo.url)
+    resp = requests.get(base_url)
+    bandsdict = resp.json()
     return bandsdict
 
+def get_bandsintown_events(bandName, id="75771b0391833569dedd2b5ceff8d2af", date="upcoming"):
+    eventdict = {}
+    base_url = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=" + id + "&date=" + date
+    resp = requests.get(base_url)
+    if resp != []:
+        eventdict = resp.json()
+        return eventdict
 
 def main():
+    sqldict = {}
     artists = ["Drake", "Hippo Campus", "STRFKR", "Mac Demarco"]
 
     for artist in artists:
-        print(get_bandsintown_artistinfo(artist))
+        eventdict = get_bandsintown_events(artist)
+        print(eventdict['name'])
 
 if __name__ == "__main__":
     main()
