a7ff906c9a36702ab54d3ca9ee9c178ef68ceaba
Virginia Pollock <pollockv@umich.edu>
2019-12-12 13:57:00 -0500

    edit functions

--- calculations.py
+++ calculations.py
@@ -4,7 +4,20 @@ import json
 import os
 import requests
 import ast
+import matplotlib.pyplot as plt
 
+def visualizationBandsInTownPie():
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    sizes = [15, 30, 45, 10]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
 
 def saveTextFile(data,filename):
     outfile = open(filename, 'w')
@@ -23,14 +36,18 @@ def joinLatLong(cur, conn):
     cur.execute("SELECT latitude, longitude FROM LongitudeForEvents JOIN LatitudeForEvents;")
 
 def calcBandsInTown(cur, conn):
+    datadict = {}
+    info = "This is a file containing percentage data for each country from our UpcomingEvents table."
+    saveTextFile(info, 'calcBandsInTown.txt')
     cur.execute("SELECT count(country) FROM UpcomingEvents WHERE country = 'United States';")
     us_number = cur.fetchall()[0][0]
     cur.execute("SELECT count(country) FROM UpcomingEvents;")
     total_number = cur.fetchall()[0][0]
     print(us_number)
     print(total_number)
-    percent = us_number / total_number * 100
-    print("{}%".format(percent))
+    percent_us = us_number / total_number * 100
+    for item in datadict:
+        saveTextFile(item, 'calcBandsInTown.txt')
         
 
 def main():
